579fb8f85098ce8baf065382217faeaf8abaa3b0
Austin Davis <austindavis4@gmail.com>
2024-02-01 00:36:49 -0700

    Multiple messages will be sent to discord if there are enough jobs that generate longer than 2k worth of text.

--- scraper/discord/discord.go
+++ scraper/discord/discord.go
@@ -7,35 +7,64 @@ import (
 	"scraper/job"
 )
 
-func SendJobsToDiscord(jobs []job.Job, webhookURL string) error {
+func generateMessages(jobs []job.Job) []string {
+	var messages []string
 	var message bytes.Buffer
+	message.WriteString("```")
+
+	for _, job := range jobs {
+		newLine := job.Link + ", " + job.Company + "\n"
+		// Discord has a 2000 character limit for messages
+		if message.Len()+len(newLine)+3 > 2000 { // +3 for the ending "```"
+			message.WriteString("```")
+			messages = append(messages, message.String())
+			message.Reset()
+			message.WriteString("```")
+		}
+		message.WriteString(newLine)
+	}
+
+	if message.Len() > 0 {
+		message.WriteString("```")
+		messages = append(messages, message.String())
+	}
+
+	return messages
+}
+
+func SendJobsToDiscord(jobs []job.Job, webhookURL string) []error {
 	if len(jobs) == 0 {
 		return nil
 	}
-	message.WriteString("```")
-	for _, job := range jobs {
-		message.WriteString(job.Link)
-		message.WriteString(", ")
-		message.WriteString(job.Company)
-		message.WriteString("\n")
+	messages := generateMessages(jobs)
+	errorChannel := make(chan error, len(messages))
+
+	go func() {
+		for _, message := range messages {
+			payload := map[string]string{
+				"content": message,
+			}
+
+			jsonPayload, err := json.Marshal(payload)
+			if err != nil {
+				errorChannel <- err
+				continue
+			}
+
+			resp, err := http.Post(webhookURL, "application/json", bytes.NewBuffer(jsonPayload))
+			if err != nil {
+				errorChannel <- err
+				continue
+			}
+			defer resp.Body.Close()
+		}
+		close(errorChannel)
+	}()
+
+	var errors []error
+	for err := range errorChannel {
+		errors = append(errors, err)
 	}
-	message.WriteString("```")
 
-	payload := map[string]string{
-		"content": message.String(),
-	}
-
-	jsonPayload, err := json.Marshal(payload)
-	if err != nil {
-		return err
-	}
-
-	resp, err := http.Post(webhookURL, "application/json", bytes.NewBuffer(jsonPayload))
-	if err != nil {
-		return err
-	}
-
-	defer resp.Body.Close()
-
-	return nil
+	return errors
 }
